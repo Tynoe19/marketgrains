ea3f4d0a243761d34f9269360b4830ae8914cacb
Tinotenda <Tinokondo2000@gmail.com>
2026-07-02 23:51:28 +0300

    fix: add misiing field in category model

--- marketgrains_backend/products/models.py
+++ marketgrains_backend/products/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
