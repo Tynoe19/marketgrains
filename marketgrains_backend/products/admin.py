0d2d349f6dab898805a0fb0488102d122498851c
Tinotenda <Tinokondo2000@gmail.com>
2026-07-12 22:51:11 +0300

    feat(products): Add PackageItem through model for packege product quanties

--- marketgrains_backend/products/admin.py
+++ marketgrains_backend/products/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import Package, Product, Category
+from .models import Package, Product, Category, PackageItem
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'is_active', 'created_at')
+    list_display = ('name', 'category', 'unit_price', 'is_active', 'created_at')
     list_filter = ('category', 'is_active')
     search_fields = ('name', 'category')
 
@@ -14,10 +14,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'created_at')
     search_fields = ('name',)
 
+class PackageItemInline(admin.TabularInline):
+    model = PackageItem
+    extra = 1
 
 @admin.register(Package)
 class PackageAdmin(admin.ModelAdmin):
     list_display = ('name', 'order_value', 'sales_value', 'profit', 'featured', 'is_active', 'created_at')
     list_filter = ('featured', 'is_active')
     search_fields = ('name',)
-    
+    inlines = [PackageItemInline]
+
--- marketgrains_backend/products/models.py
+++ marketgrains_backend/products/models.py
@@ -14,17 +14,18 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     image = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=True)
+    
 
     def __str__(self):
         return self.name
 class Package(models.Model):
-    products= models.ManyToManyField(Product, related_name='packages')
+    products= models.ManyToManyField(Product, through="PackageItem", related_name='packages')
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     order_value = models.DecimalField(max_digits=10, decimal_places=2)
@@ -37,4 +38,12 @@ class Package(models.Model):
 
     def __str__(self):
         return self.name
+class PackageItem(models.Model):
+    package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name='package_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    product_weight = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)  # Weight in kg
 
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in {self.package.name}"
